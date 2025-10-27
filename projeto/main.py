77059cb3f6d4e155d79d7270fbf7218220ff9141
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2025-10-27 11:01:33 -0300

    new commit

--- projeto/main.py
+++ projeto/main.py
@@ -1,1 +1,14 @@
-import mediapipe
+import mediapipe as mp
+import cv2
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpMaos = mp.solutions.hands 
+hands = mpMaos.Hands()
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
