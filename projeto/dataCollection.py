f11cfd7ba03ee7dee55852b2f6c504bfd4f3fc77
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2025-11-29 02:29:18 -0300

    new commit

--- projeto/dataCollection.py
+++ projeto/dataCollection.py
@@ -1,9 +1,16 @@
 import cv2
 import math
 from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+
+
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
+
+dist = 20
+TAMANHOIMG = 300
+
 
 window_name = "Image"
 while True:
@@ -17,15 +24,51 @@ while True:
         hand = hands[0]
         #hand2 = hand[1]
         posiX, posiY, largura, altura = hand['bbox']
+
+
+
+
+        #Criando uma matriz numpy -> com valor (1,1,1) para cada pixel
+        #np.ones(<tamanho em pixels, largura em pixels, canal de cor>, <tipo de dados dois pixels>)
+        #imageVermelha -> ([1,1,1], canalTyp)
+        imageVermelha = np.ones((TAMANHOIMG, TAMANHOIMG, 3), np.uint8)
+        #faz com que o canal R seja 255
+        imageVermelha[:,:, 2] *= 255
+
+
+
+
         #print(f"({posiX},{posiY})")
-        print(f"y: {posiY + altura}, x: {posiX + largura}")
-        #print(f"w: {w}")
-        #print(f"h: {h}")
-        #error->
-        if(posiY > 0 and posiX > 0):
-            recorteImagem = img[posiY: posiY + altura, posiX: posiX + largura]
+        #print(f"altura: {altura}, largura: {largura}")
+        
+
+        #Codigo para verificar se não está ocorrendo estouro de borda
+        if(posiY - dist> 0 and posiX - dist > 0):
+            recorteImagem = img[posiY - dist: posiY + altura + dist, posiX - dist: posiX + largura + dist]
+
+            #shape -> retorna uma matriz de três valores [altura, largura, canal]
+            recorteImagemTamanho = recorteImagem.shape
+
+            #Verifica de o tamanho da imagem da mão não ultrapassa a grade de imagemVermelha
+            if(recorteImagemTamanho[0] < TAMANHOIMG and recorteImagemTamanho[1] < TAMANHOIMG):
+
+                comparaDimen = altura/largura
+
+                if (comparaDimen > 1):
+                    k = TAMANHOIMG/altura
+                    larguraCall = math.ceil(k*largura)
+
+                    imgResize = cv2.resize(recorteImagem, (larguraCall, TAMANHOIMG))
+                    imgResizeTamanho = imgResize.shape
+
+                    #Clipando a imagem recorte na imagemVermelha -> imagemVermelha[largura, altura] = recorteImagem
+                    imageVermelha[0:imgResizeTamanho[0], 0:imgResizeTamanho[1]] = recorteImagem
+
             cv2.imshow("recorteImagem", recorteImagem)
-    #mostra a img em uma janela GUI
+            cv2.imshow("imagemBranca", imageVermelha)
+
+
+    #mostra a img em uma janela GUI (Graphcs User Interface)
     cv2.imshow("Image", img)
     #delay de 1 ms
     cv2.waitKey(1)
